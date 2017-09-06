1d85953306944c5ee2027c52ddf9501646e628a0
H9458 <H9458@student.jamk.fi>
2017-09-06 10:57:46 +0300

    Labrat project added

---
+++ Labrat/Lab01.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Olio-ohjelmonnin viikkotehtävät Labra1
+//Lassi Markkola
+namespace Labrat
+{
+    class Lab01
+    {
+        public static void Tehtava01 ()
+            {
+            //Tee ohjelma, joka tulostaa käyttäjän antamaa lukua
+            Console.WriteLine("Työn alla...");
+            }
+    }
+}
