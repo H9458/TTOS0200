520f9b8b21296ed6025d3a7c0d917968f80fd2f9
H9458 <H9458@student.jamk.fi>
2017-10-04 11:06:46 +0300

    Demos added

---
+++ OOdemot/DemoKapselointi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOdemot
+{
+    class Thing
+    {
+        //julkinen jäsenmuuttuja != ominaisuus
+        //public string Name;
+        //parempi vaihtoehto, ominaisuus AutoImplemented Propertyn avulla
+        //public string Name { get; set; }
+        //ToString -metodi käyttää jäsenmuuttujaa tai propertyä
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        //Constructors
+        public Thing()
+        {
+            name = "nobody here";
+        }
+        public override string ToString()
+        {
+            return "nimeni on: " + Name;
+        }
+    }
+    class Program
+    {
+        public static void main(string[] args)
+        {
+            Thing t = new Thing();
+            t.Name = "jotakin";
+            Console.WriteLine(t.Name);
+            Console.WriteLine(t.ToString());
+        }
+    }
+    
+}
---
+++ OOdemot/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOdemot
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AsioID { get; set; }
+        public Student()
+        {
+
+        }
+        public Student (string fname, string lname)
+        {
+            FirstName = fname;
+            LastName = lname;
+        }
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + "ASIOID " + AsioID;
+        }
+    }
+    public static void main()
+    {
+        Student s = new Student();
+        s.FirstName = "Esa";
+        s.LastName = "Salmikangas";
+        Student s2 = new Student("Pekka", "Perjantai");
+        //luodaan kokoelma Student-olioita, käytetään List
+        List<Student> students = new List<Student>();
+        students.Add(s);
+        students.Add(s2);
+        students.Add(new Student("Arska", "Aaltonen"));
+        //näytetään oppilaat
+        Console.WriteLine("Luokan oppilaat: ");
+        foreach (Student item in students)
+        {
+            Console.WriteLine(item.ToString());
+        }
+    }
+}
